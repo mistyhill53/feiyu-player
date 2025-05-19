aafbff322fb3baa1a7c19ae071227a99e331d8aa
mistyhill53 <ssh@ik.me>
2025-05-19 15:39:59 +0800

    Update default.ts

--- packages/feiyu/src/data/default.ts
+++ packages/feiyu/src/data/default.ts
@@ -1,12 +1,76 @@
 import { FeiyuConfig } from './config/types';
 
 export default {
-  proxy: 'https://example.vercel.app/api/proxy',
+  proxy: 'https://feiyu-player-52y2.vercel.app/api/proxy',
   videoSources: [
     {
-      key: '视频源',
-      api: 'https://api1.example.com/api.php/provide/vod/at/xml',
+      "key": "CK资源",
+      "api": "https://www.ckzy1.com/api.php/provide/vod/at/xml"
     },
+    {
+      "key": "jkun资源",
+      "api": "https://jkunzyapi.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "百万资源",
+      "api": "https://api.bwzym3u8.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "souav资源",
+      "api": "https://api.souavzy.vip/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "155资源",
+      "api": "https://155api.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "lsb资源",
+      "api": "https://apilsbzy1.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "黄色仓库",
+      "api": "https://hsckzy.vip/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "玉兔资源",
+      "api": "https://yutuzy10.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "杏吧资源",
+      "api": "https://xingba111.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "小鸡资源",
+      "api": "https://api.xiaojizy.live/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "极品X资源",
+      "api": "https://jipinzyapi.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "丝袜资源",
+      "api": "https://siwazyw.tv/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "色猫资源",
+      "api": "https://caiji.semaozy.net/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "易看资源",
+      "api": "https://api.ekanapi.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "奶香香资源",
+      "api": "https://naixxzy.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "辣椒资源",
+      "api": "https://apilj.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "大地资源",
+      "api": "https://dadiapi.com/api.php/provide/vod/at/xml"
+    }
   ],
   hotMovies: [
     {
